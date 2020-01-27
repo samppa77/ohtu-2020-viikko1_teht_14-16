1e722591ae8ef63a0648af4d2d91a0d2b9f7a68e
samppa77 <sluukkai@gmail.com>
2020-01-28 00:23:16 +0200

    test 2b korjaus

--- NhlStatistics1/src/test/java/ohtuesimerkki/StatisticsTest.java
+++ NhlStatistics1/src/test/java/ohtuesimerkki/StatisticsTest.java
@@ -70,49 +70,42 @@ public class StatisticsTest {
 
     }
 
-    
     @Test
     public void test2a() {
 
 //        List<Player> players2 = new ArrayList<>();
 //        players2.add(new Player("Lemieux", "PIT", 45, 54));
-
 //        players2.add(new Player("Semenko", "EDM", 4, 12));
 //        players2.add(new Player("Kurri", "EDM", 37, 53));
 //        players2.add(new Player("Gretzky", "EDM", 35, 89));
-
 //        String tiimi = stats.team("PIT").getTeam();
-
 //        ArrayList<Player> players3 = stats.team("PIT");
         List<Player> players3 = stats.team("PIT");
         Player pelaaja = players3.get(0);
-        
+
         String nimi = players3.get(0).getName();
         /*
                 
                 .getName());
         System.out.println(players3);
-*/
+         */
 
 //        System.out.println("Joukkue on: " + tiimi);
-
-
         assertEquals("Lemieux", nimi);
 
     }
 
-
-
+    @Test
     public void test2b() {
 
 //        List<Player> players2 = new ArrayList<>();
 //        players2.add(new Player("Lemieux", "PIT", 45, 54));
+        ArrayList<Player> vertaa = new ArrayList<Player>();
 
-        List<Player> players = stats.team("OTT");        
-        
-        assertNull(players);
+        List<Player> players = stats.team("OTT");
 
+//        assertNull(players);
+        assertEquals(vertaa, players);
 
     }
 }
-
